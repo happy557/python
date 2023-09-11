a50324847efe63f015e159efbf8bb0f0f071113a
ranzer094@gmail.com <amaljithkaliyodath1@gmail.com>
2023-09-11 13:21:18 +0530

    first commit

---
+++ start.py
@@ -0,0 +1,31 @@
+# print("welcome")
+# print(10)
+# print(10+20)
+# num1=20
+# num2=10
+# print(num1+40)
+# print(num1+num2)
+# a=10
+# a=20
+# print(a)
+# a=10
+# b=20
+# c=a
+# a=b
+# b=c
+# print(a)
+# print(b)
+# a,b=10,20
+# b,a=a,b
+# print(a)
+# print(b)
+# a="python"
+# print(type(a))
+# print()
+# a=input()
+# print(a)
+# a=input("Enter a number")
+# print(a, "is a number")
+a=int(input("Enter a number"))
+b=int(input("Enter a number"))
+print(a+b)
---
+++ start1.py
@@ -0,0 +1,2 @@
+a=int(input("date of birth"))
+print(2023-a)
