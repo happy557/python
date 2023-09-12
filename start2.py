340525a3fa4c3fc88c25c91d1e4a081d7dd28991
ranzer094@gmail.com <amaljithkaliyodath1@gmail.com>
2023-09-12 12:59:55 +0530

    commit

---
+++ start2.py
@@ -0,0 +1,19 @@
+# a=10
+# a+=20
+# print(a)
+# a=10
+# print(a==10 and a<5) 
+# b=20
+# print(b==20 and b<25)
+# a==10
+# print(not(a==10))
+# c="python"
+# print("y"in c)
+# print("y"not in c)
+# d="Hello"
+# e="hello"
+# print (a is b)
+"""a= let me tell a kutti story
+story.. story.. not.. boring...
+lalaa lalaa lalaa la la la la """
+print(a)
