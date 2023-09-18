da5b31ba663223e6476bdd51170a7dd22e88c17b
ranzer094@gmail.com <amaljithkaliyodath1@gmail.com>
2023-09-18 13:08:43 +0530

    add

---
+++ start6.py
@@ -0,0 +1,42 @@
+# 3
+# a=int(input("enter first number"))
+# b=int(input("enter second number"))
+# if a>b:
+#     print(a,"is largest")
+# else:
+#     print(b,"is largest")
+
+# 4
+# x=int(input("enter your age"))
+# if(x<=18):
+#     print("you are a minor")
+# elif(x>18 and x<65):
+#     print("you are an adult")
+# else:
+#     print("you are a senor citizen")
+
+# 6
+# x=int(input("enter first no:"))
+# y=int(input("enter second no:"))
+# if(x%y==0):
+#     print(x,"is divisible by",y)
+# else:
+#     print(x,"is not divisible by",y)
+
+# 7
+# m=int(input("enter first number:"))
+# n=int(input("enter second number:"))
+# s=m+n
+# if(s%2==0)
+#     print(s,"is even")
+# else:
+#     print(s,"is odd2")
+
+2
+x=input("enter a letter:")
+v=("a","e", "i","o","u","A","E","I","O","U")
+if(x in v):
+    print(x,"is a vowel")
+else:
+    print(x,"is a consonant")
+    
