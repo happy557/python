e94c49f420f9dd7992a1ba614f071f7a2ad09a85
ranzer <ranzer094@gmail.com>
2023-09-25 13:19:48 +0530

    loop

--- loop.py
+++ loop.py
@@ -1,8 +1,10 @@
-a=[1,2,3,4,5,6]
-print(a)
-for i in a:
-    print(i)
+# a=[1,2,3,4,5,6]
+# print(a)
+# for i in a:
+#     print(i)
     
 a=int(input("enter a number"))
-for i in range(1,11):
-    print(i,'x',a,'=',i*a)
+# for i in range(1,11):
+#     print(i,'x',a,'=',i*a)
+for i in range(1,a+1):
+        print(i*" *")
---
+++ loop2.py
@@ -0,0 +1,13 @@
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j,end=' ')
+#     print(' ')
+    
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end=' ')
+    print(' ')
+for i in range(1,6):
+    for j in range(-1,0,6):
+        print(j,end=' ')
+    print(' ')
