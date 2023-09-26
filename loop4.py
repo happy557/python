8a511916cc2fdabafbec9c4097d27121ef8140e9
ranzer <ranzer094@gmail.com>
2023-09-26 13:28:54 +0530

    add

---
+++ loop4.py
@@ -0,0 +1,23 @@
+# a=[2,9,4,3,5,1,7,6,8]
+# a.sort()
+# print(a[-2]) 
+# print(a[1])
+# a=list(input("Enter the numbers"))
+# a.sort()
+# print(a[-2])
+# print(a[1])
+# a=int(input("enter the numbers:"))
+# b=[]
+# for i in range(a):
+#     c=int(input("enter the elements"))
+#     b.append(
+# b.sort()
+# print(b[-2])
+# print(b[1])
+string=input("enter a string")
+if(string==string[::-1]):
+    print(len(string)* " @")
+    print("string is a palindrome")
+else:
+    print("string is not a palindrome")
+    
