7db61016bd4a88c4ca40650c254cc0e216f232ae
ranzer <ranzer094@gmail.com>
2023-10-05 13:08:19 +0530

    add

--- loop9.py
+++ loop9.py
@@ -15,7 +15,7 @@ while True:
             "name":name,
             "age":age,
             "contact_number":contact_number,
-            "account number":account_number,
+            "account_number":account_number,
             "balance":0
             }
         customers.append(new_customer)
@@ -23,6 +23,32 @@ while True:
     elif choice=='2':
         print("\ncustomers list")
         for customer in customers:
-            print("\nName:{customer['Name']}\nAge: {customer['Age']}\ncontact:{customer['contact_number']}\n Account Number:{customer['account_number']}\nBalance:${customer['balance']}")
-                
-                    
+            print(f"\nName:{customer['name']}\nAge: {customer['age']}\ncontact:{customer['contact_number']}\nAccount Number:{customer['account_number']}\nBalance:{customer['balance']}")
+    elif choice=='3':
+        account_number=input("enter account number:")
+        amount=float(input("enter deposit amount:"))
+        for customer in customers:
+            if customer["account_number"]==account_number:
+                customer["balance"]+=amount
+            print(f"Deposit {amount} Successful. New balance:{customer['balance']}")
+            break
+        else:
+           print("Customer not found") 
+    elif choice=='4':
+        account_number=input("enter account number:")
+        amount=float(input("enter withdrawal amount:"))
+        for customer in customers:
+            
+           if amount>customer["balance"]:
+            print("insufficent Fund")
+           else:
+            customer["balance"]-=amount
+           print(f"Withdraw {amount} Successful. New balance:{customer['balance']}")
+           break
+        else:
+         print("Customer not found") 
+    elif choice=='5':
+        print("exiting the program.")
+        break
+    else:
+        print("invalid choice.Please enter a valid option.")
