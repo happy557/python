0f29bb4c7ae61fd46eea9faee53f078579c91fe4
ranzer <ranzer094@gmail.com>
2023-10-16 13:17:27 +0530

    add

---
+++ leet8.py
@@ -0,0 +1,7 @@
+a="1010"
+b="1011"
+i=int(a,2)
+j=int(b,2)
+m=i+j
+k=bin(m)[2:]
+print(k)
---
+++ list.py
@@ -0,0 +1,71 @@
+#lab1
+# a={1,2.9,'string',True,{1,2}}
+
+#lab2
+# numbers=[1,2,3,4,5,6,7]
+# sub_list=numbers[1:5]
+# print(sub_list)
+
+#lab3
+# list1=[1,2,3]
+# list2=[4,5,6]
+# list3=list1+list2
+# print(list3)
+
+#lab4
+# fruits=['apple','banana','cherry']
+# fruits.append('kiwi')
+# fruits.insert(1,'orange')
+# fruits.extend(['grape','blueberry'])
+# fruits.remove('banana')
+# print(fruits)
+
+#lab5
+# numbers=[5,2,8,1,3]
+# ascending_order=sorted(numbers)
+# descending_order=sorted(numbers[::-1])
+# print(descending_order)
+
+#lab6
+# my_list=[1,2.9,'string',True,[1,2]]
+# last_element=my_list[-1]
+# print(last_element)
+
+#lab8
+# fruits=['apple','banana','cherry']
+# apple_count=fruits.count('apple')
+
+#lab9
+# numbers=[5,2,8,1,3]
+# reversed_numbers=numbers[::-1]
+
+#lab10
+# original_list=[1,2,3]
+# copied_list=original_list.copy()
+# copied_list.append(4)
+# print(original_list,copied_list)
+
+#lab16
+# numbers=[5,2,8,1,3]
+# min_value=min(numbers)
+# max_value=max(numbers)
+
+#lab17
+# fruits=['apple','banana','cherry']
+# banana_exits='banana' in fruits
+
+#lab18
+# fruits=['apple','banana','cherry']
+# length=len(fruits)
+
+#lab19
+# numbers=[5,2,8,1,3]
+# sum=sum(numbers)
+
+#lab20
+fruits=['apple','banana','cherry']
+fruits[1]='grapefruits'
+print(fruits)
+
+
+
